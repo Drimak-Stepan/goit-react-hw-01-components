5af7a46b401656bc8792700be67890c935281481
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2023-01-13 09:04:54 +0200

    Update FriendListItem.jsx
    Id delete

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
-export default function FriendListItem({ avatar, name, isOnline, id }) {
+export default function FriendListItem({ avatar, name, isOnline }) {
   return (
-    <li className={css.item} key={id}>
+    <li className={css.item}>
       <span className={`${css.status} ${css[isOnline]}`}></span>
       <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
